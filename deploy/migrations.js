cdd681c2fc22f359299b874edf3e136757ccb7ea
Scott Stroz <scott.stroz@oracle.com>
2023-10-12 08:47:26 -0400

    Finxing merge conflicts

---
+++ deploy/migrations.js
@@ -0,0 +1,15 @@
+import DbUtils from "../utils/dbUtils.js";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const dbUtils = new DbUtils(
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    process.env.DB_HOST,
+    process.env.DB_PORT,
+    process.env.DB_SCHEMA
+)
+
+await dbUtils.runLatestMigration();
+
+console.log('Migrations complete')
