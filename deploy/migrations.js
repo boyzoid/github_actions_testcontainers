0fedf6293bbc890b59b1f82b54b3d79637524ad5
Scott Stroz <scott.stroz@oracle.com>
2023-10-12 08:58:28 -0400

    Updates to Node code for migrations

--- deploy/migrations.js
+++ deploy/migrations.js
@@ -2,14 +2,20 @@ import DbUtils from "../utils/dbUtils.js"
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const dbUtils = new DbUtils(
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    process.env.DB_HOST,
-    process.env.DB_PORT,
-    process.env.DB_SCHEMA
-)
+async function run(){
+    const dbUtils = new DbUtils(
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        process.env.DB_HOST,
+        process.env.DB_PORT,
+        process.env.DB_SCHEMA
+    )
 
-await dbUtils.runLatestMigration()
+    console.log(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST, process.env.DB_PORT, process.env.DB_SCHEMA)
 
-console.log('Migrations complete')
+    await dbUtils.runLatestMigration()
+
+    console.log('Migrations complete')
+}
+
+run()
